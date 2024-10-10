ba39749f379d9014b1a23de05bee268feb0ac343
higashi <higashiyuga1@gmail.com>
2024-10-10 11:31:19 +0900

    make cell2

--- puzzle_board.gd
+++ puzzle_board.gd
@@ -5,6 +5,7 @@ var grid_row
 var dx = 4000
 var dy = 1000
 var piece = []
+var grid_n = []
 func _ready() -> void:
 	grid_column = 20
 	grid_row = 15
@@ -15,11 +16,28 @@ func _ready() -> void:
 			child.position.x = -1000000
 			valid_children.append(child)
 	for i in range(grid_column):
+		var arr = []
 		for j in range(grid_row):
-			var adc = valid_children[randi() % valid_children.size()].duplicate();
+			var canset = []
+			for k in range(5):
+				canset.append(true)
+			if(i>=2):
+				if(grid_n[i-1][j]==grid_n[i-2][j]):
+					canset[grid_n[i-1][j]] = false
+			if(j>=2):
+				if(arr[j-1]==arr[j-2]):
+					canset[arr[j-1]] = false
+			var rng = []
+			for k in range(5):
+				if(canset[k]):
+					rng.append(k)
+			var nval = rng[randi() % rng.size()]
+			arr.append(nval)
+			var adc = valid_children[nval].duplicate();
 			adc.position = Vector2(i*500+500+dx,j*500+500+dy)
 			piece.append(adc)
 			add_child(adc)
+		grid_n.append(arr)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
