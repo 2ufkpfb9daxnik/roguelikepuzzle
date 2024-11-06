56720279058cb2d7d75e108ee5b942c80bbac33d
2ufkpfb9daxnik <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2024-11-06 16:48:33 +0900

    buff

---
+++ buff_card.gd
@@ -0,0 +1,19 @@
+# BuffCard.gd
+extends Button
+
+signal buff_selected
+
+# バフデータを保持する変数
+var buff_data
+
+# 初期設定メソッド
+func setup(data):
+	buff_data = data
+	# バフの情報を設定
+	$Title.text = buff_data["name"]
+	$Description.text = buff_data["description"]
+	$Icon.texture = load(buff_data["icon_path"])
+
+# ボタンが押されたときにシグナルを発信
+func _on_button_pressed():
+	emit_signal("buff_selected", buff_data)
