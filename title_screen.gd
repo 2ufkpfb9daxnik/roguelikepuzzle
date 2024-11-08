4fbfdc0d7bfca98620da872f64cc687a19be0117
9k3pthhurh8f9z <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2024-11-08 23:14:02 +0900

    title

--- title_screen.gd
+++ title_screen.gd
@@ -4,6 +4,7 @@ var isclicked = false
 var interval = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_node("titlebgm").play()
 	pass # Replace with function body.
 func _on_button_pressed() -> void:
 	if(isclicked):
